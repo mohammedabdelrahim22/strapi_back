02bba316261a9a7984dad99d2e287a02af68082d
mohammedabdelrahim22 <mohammedabdelrahim42@gmail.com>
2023-06-02 05:01:02 +0300

    ubdates

--- config/plugins.js
+++ config/plugins.js
@@ -3,10 +3,10 @@ module.exports = ({ env }) => ({
         enabled: true,
         config: {
             deployHook:
-                "https://api.vercel.com/v1/integrations/deploy/prj_<deploy-hook>",
-            apiToken: "<vercel-api-token>",
-            appFilter: "your-app-name-on-vercel",
-            teamFilter: "your-team-id-on-vercel",
+                "https://api.vercel.com/v1/integrations/deploy/prj_https://api.vercel.com/v1/integrations/deploy/prj_Op0vaQJ6DFkbS1niHlTRBVHJNlLV/ba70mL5N9R",
+            apiToken: "epod0kGLFjhwPD1AaQFt5pJz",
+            appFilter: "strapi",
+            teamFilter: "WRriJjSimnv6pxLgCJJt4cO0",
             roles: ["strapi-super-admin", "strapi-editor", "strapi-author"],
         },
     },
