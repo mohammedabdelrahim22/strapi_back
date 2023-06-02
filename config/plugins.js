4ca1b3df1b62f3ee5691e273b371e102b8968714
mohammedabdelrahim22 <mohammedabdelrahim42@gmail.com>
2023-06-02 04:35:19 +0300

    updates

--- config/plugins.js
+++ config/plugins.js
@@ -1,16 +1,13 @@
 module.exports = ({ env }) => ({
-    "netlify-deployments": {
+    "vercel-deploy": {
         enabled: true,
         config: {
-            accessToken: "iL6tKEwk3MPHLf_nfD1uNa2TY5DvDKZtrTI1zP2ehmk",
-            sites: [
-                {
-                    name: 'Site 1',
-                    id: "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx",
-                    buildHook: "https://api.netlify.com/build_hooks/64793a868c2f8134237c6e94",
-                    branch: 'main' // optional
-                }
-            ]
+            deployHook:
+                "https://api.vercel.com/v1/integrations/deploy/prj_<deploy-hook>",
+            apiToken: "<vercel-api-token>",
+            appFilter: "your-app-name-on-vercel",
+            teamFilter: "your-team-id-on-vercel",
+            roles: ["strapi-super-admin", "strapi-editor", "strapi-author"],
         },
     },
 });
